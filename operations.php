8f2c15dedd2c7a7dc6eff5f6366fc8207ca0f14b
maxmusiichuk <poprobui_ugadaI1>
2020-07-21 11:26:54 +0300

    Add file with operations with variables

--- operations.php
+++ operations.php
@@ -1,1 +1,15 @@
 <?php
+//operations for variables
+$x = 1;
+$y = pi();
+$a = ($x * 2) / 100;
+
+echo `$x` . gettype($x);
+echo "<br>" . `$y` . gettype($y);
+echo "<br>" . `$a` . gettype($a);
+
+if (isset($x)) echo 'Variable $x exist '.'<br>';
+
+unset($x);
+if (isset($x)==false) echo'Variable $x don`t exist any more'.'<br>';
+echo gettype($x);
