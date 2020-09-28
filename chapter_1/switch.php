81f33849cebb0fe2b5f5e762937431395604bcc1
maxmusiichuk <musiichuk646@gmail.com>
2020-09-28 13:52:06 +0300

    ->comment for checkout

---
+++ chapter_1/switch.php
@@ -0,0 +1,19 @@
+<?php
+
+$x = 5;
+
+switch ($x){
+
+    case 0:
+        echo "x=0";
+        break;
+    case 1:
+        echo "x=1";
+        break;
+    case 2:
+        echo "x=2";
+        break;
+    default:
+        echo "Something wrong";
+        break;
+}
