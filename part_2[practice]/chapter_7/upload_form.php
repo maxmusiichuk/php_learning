0108206b4705e1889baa6eb43451c7d5bde7454c
maxmusiichuk <musiichuk646@gmail.com>
2020-11-25 18:34:48 +0200

    ->created file with simple upload form

---
+++ part_2[practice]/chapter_7/upload_form.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<FORM ENCTYPE="multipart/form-data" ACTION="upload_form.php" method="post">
+    Choose file: <input name="upfile" type="file">
+    <input type="submit" value="upload">
+</FORM>
+</body>
+</html>
